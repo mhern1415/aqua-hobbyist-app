cd789dd12f449d8d4298ea971818f6826efaf366
Mark Hernandez <m.hern1415@gmail.com>
2019-11-06 22:45:14 +0000

    changed seed files and added sigup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,30 @@ class UsersController < ApplicationController
     erb :'/users/show'
   end
 
-  
+  get '/signup' do
+    #render my sign form
+    erb :'/users/signup'
+  end
+
+  # post sign up route that recieve input data from user, create the user, and logs user in
+  post '/users' do
+    # will eventually need to add validations to confirm all inputs are filled out before creating user
+    @user = User.create(params)
+    # post sign up route to create user using params and add key/value pair to sessions hash
+    session[:user_id] = @user.id
+    # redirect to user profile
+    redirect "/users/#{@user.id}"
+  end
+
+  # LOG OUT
+  # get logout that clears the session hash
+  get '/logout' do
+    #binding.pry
+    session.clear
+    #binding.pry
+    # redirect to home/landing page
+    redirect '/'
+  end
   
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 User.destroy_all
 Tank.destroy_all
 
-mark = User.create(name: "Mark", email: "mark@mark.com", image_url: "https://imgur.com/2WyHhuv", bio: "i love plants", password: "pw")
+mark = User.create(name: "Mark", email: "mark@mark.com", image_url: "https://live.staticflickr.com/65535/49025620408_0c03d21de4_b.jpg", bio: "i love plants", password: "pw")
 
-ingrid = User.create(name: "Ingrid", email: "ingrid@ingrid.com", image_url: "https://imgur.com/2WyHhuv", bio: "i love fish", password: "pw")
+ingrid = User.create(name: "Ingrid", email: "ingrid@ingrid.com", image_url: "https://live.staticflickr.com/65535/49025620408_0c03d21de4_b.jpg", bio: "i love fish", password: "pw")
 
 Tank.create(title: "20 Gallon New", image_url: "https://i.imgur.com/w2pzhXm.jpg", size: "20G", flora: "Ludwigia Repens Rubin", user_id: ingrid.id)
 
 Tank.create(title: "20 Gallon Newer", image_url: "https://i.imgur.com/w2pzhXm.jpg", size: "20Gallons", flora: "Limnophila Aromatica", user_id: ingrid.id)
 
 Tank.create(title: "20 Gallon Newest", image_url: "https://i.imgur.com/w2pzhXm.jpg", size: "20Gal", flora: "Ludwigia Super Red", user_id: mark.id)
+
