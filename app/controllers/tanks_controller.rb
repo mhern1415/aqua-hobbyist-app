8b894ab399072b4883d74a7a334b737b9fcb2515
Mark Hernandez <m.hern1415@gmail.com>
2019-11-07 01:33:54 +0000

    added tank show page

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,11 +1,14 @@
 class TanksController < ApplicationController
   
-get "/tanks" do
-  @tanks = Tank.all 
-  erb :"tanks/index"
-end
+  get "/tanks" do
+    @tanks = Tank.all 
+    erb :"tanks/index"
+  end
   
-  
+  get '/tanks/:id' do
+    @tank = Tank.find(params[:id])
+    erb :"/tanks/show"
+  end
   
   
   
