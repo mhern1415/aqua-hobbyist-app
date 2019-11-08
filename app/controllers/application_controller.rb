fa8e11f864bb153eb88591500779eee72b696859
Mark Hernandez <m.hern1415@gmail.com>
2019-11-08 00:42:24 +0000

    modified css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret_session" 
-    
+    register Sinatra::Flash
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:message] = "Welcome back, #{user.name}!"
       redirect "/users/#{user.id}"
     else
+      flash[:error] = "Whoops! Username or password incorrect"
       redirect "/login"
     end
   end
